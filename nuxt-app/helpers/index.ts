78e51eb0bbf601316e0442a23359e166df48d001
Fabian Hiller <hillerfabian11@gmail.com>
2021-09-30 21:17:02 +0200

    Add getCookie und setCookie helper

--- nuxt-app/helpers/index.ts
+++ nuxt-app/helpers/index.ts
@@ -1,4 +1,6 @@
+export * from './getCookie';
 export * from './getHashCode';
 export * from './getImageSrcSet';
 export * from './getPodcastTitleDivider';
 export * from './getPodcastTypeString';
+export * from './setCookie';
---
+++ nuxt-app/helpers/setCookie.ts
@@ -0,0 +1,12 @@
+/**
+ * Helper function to set a new browser cookie.
+ *
+ * @param name The name of the cookie.
+ * @param value The value of the cookie.
+ * @param days The lifetime of the cookie in days.
+ */
+export function setCookie(name: string, value: string, days: number) {
+  const maxAge = 60 * 60 * 24 * days;
+  const domain = window.location.hostname.split('.').slice(-2).join('.');
+  document.cookie = `${name}=${value}; max-age=${maxAge}; domain=${domain}; path=/; samesite=strict; secure`;
+}
