d6cddee7cc922a432759938c42f1f1ada3cc0019
Fabian Hiller <hillerfabian11@gmail.com>
2021-10-02 23:18:30 +0200

    Fix types of useStrapi composable

--- nuxt-app/composables/useStrapi.ts
+++ nuxt-app/composables/useStrapi.ts
@@ -13,47 +13,49 @@ import {
 } from 'shared-code';
 import { getHashCode } from '../helpers';
 
-export function useStrapi(route: 'members'): Ref<StrapiMember[]>;
+export function useStrapi(route: 'members'): Ref<StrapiMember[] | null>;
 export function useStrapi(
   route: 'members',
   param?: Ref<`/${string}`>
 ): Ref<StrapiMember | null>;
-export function useStrapi(route: 'speakers'): Ref<StrapiSpeaker[]>;
+export function useStrapi(route: 'speakers'): Ref<StrapiSpeaker[] | null>;
 export function useStrapi(
   route: 'speakers',
   param: Ref<`?_limit=${number}`>
-): Ref<StrapiSpeaker[]>;
+): Ref<StrapiSpeaker[] | null>;
 export function useStrapi(
   route: 'speakers',
   param?: Ref<`/${string}`>
 ): Ref<StrapiSpeaker | null>;
-export function useStrapi(route: 'podcasts'): Ref<StrapiPodcast[]>;
+export function useStrapi(route: 'podcasts'): Ref<StrapiPodcast[] | null>;
 export function useStrapi(
   route: 'podcasts',
   param: Ref<`?${string}&_sort=published_at:DESC` | `?_limit=${number}`>
-): Ref<StrapiPodcast[]>;
+): Ref<StrapiPodcast[] | null>;
 export function useStrapi(
   route: 'podcasts',
   param: Ref<`/${string}`>
 ): Ref<StrapiPodcast | null>;
-export function useStrapi(route: 'meetups'): Ref<StrapiMeetup[]>;
+export function useStrapi(route: 'meetups'): Ref<StrapiMeetup[] | null>;
 export function useStrapi(
   route: 'meetups',
   param: Ref<`?_limit=${number}`>
-): Ref<StrapiMeetup[]>;
+): Ref<StrapiMeetup[] | null>;
 export function useStrapi(
   route: 'meetups',
   param: Ref<`/${string}`>
 ): Ref<StrapiMeetup | null>;
-export function useStrapi(route: 'picks-of-the-day'): Ref<StrapiPickOfTheDay[]>;
+export function useStrapi(
+  route: 'picks-of-the-day'
+): Ref<StrapiPickOfTheDay[] | null>;
 export function useStrapi(
   route: 'picks-of-the-day',
   param: Ref<`?_limit=${number}`>
-): Ref<StrapiPickOfTheDay[]>;
-export function useStrapi(route: 'home-page'): Ref<StrapiHomePage>;
-export function useStrapi(route: 'about-page'): Ref<StrapiAboutPage>;
-export function useStrapi(route: 'podcast-page'): Ref<StrapiPodcastPage>;
-export function useStrapi(route: 'meetup-page'): Ref<StrapiMeetupPage>;
+): Ref<StrapiPickOfTheDay[] | null>;
+export function useStrapi(route: 'home-page'): Ref<StrapiHomePage | null>;
+export function useStrapi(route: 'about-page'): Ref<StrapiAboutPage | null>;
+export function useStrapi(route: 'podcast-page'): Ref<StrapiPodcastPage | null>;
+export function useStrapi(route: 'meetup-page'): Ref<StrapiMeetupPage | null>;
 export function useStrapi(route: string, param?: Ref<string>) {
   const hashCode = computed(() =>
     param?.value ? `${getHashCode(param.value)}` : 'list'
