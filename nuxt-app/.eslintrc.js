c9f9a8cccc0d6e3ad3be96ea8a88b7f4b9ef3fef
Fabian Hiller <hillerfabian11@gmail.com>
2021-12-07 17:45:30 +0100

    Update packages at cloud-functions and nuxt-app

--- nuxt-app/.eslintrc.js
+++ nuxt-app/.eslintrc.js
@@ -2,15 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
+    '@nuxtjs',
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'prettier',
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    'vue/multi-word-component-names': 0,
+  },
+};
