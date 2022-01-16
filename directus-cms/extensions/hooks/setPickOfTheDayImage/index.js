2e1a6e61aec6759e6d1bfd314a0058c6915c7e69
Fabian Hiller <hillerfabian11@gmail.com>
2022-01-16 15:40:57 +0100

    Refactor hook and replace filter with action

--- directus-cms/extensions/hooks/setPickOfTheDayImage/index.js
+++ directus-cms/extensions/hooks/setPickOfTheDayImage/index.js
@@ -7,25 +7,28 @@ const postSlackMessage = require('../../../helpers/postSlackMessage');
 const HOOK_NAME = 'setPickOfTheDayImage';
 
 module.exports = (
-  { filter },
-  { env, exceptions: { BaseException }, logger, services: { FilesService } }
+  { action },
+  {
+    env,
+    exceptions: { BaseException },
+    logger,
+    services: { FilesService, ItemsService },
+  }
 ) => {
   /**
-   * It handles the filter logic that sets the "image" field on
+   * It handles the action logic that sets the "image" field on
    * created or updated pick of the day items, if necessary.
    *
-   * @param data The filter data.
-   *
-   * @returns The new payload.
+   * @param data The action data.
    */
-  async function handleFilter({ payload, metadata, context }) {
+  async function handleAction({ payload, metadata, context }) {
     try {
       // Log start info
-      logger.info(`${HOOK_NAME} hook: Start filter function`);
+      logger.info(`${HOOK_NAME} hook: Start action function`);
 
-      // If collection is "picks_of_the_day" and payload contain "website_url",
-      // create screenshot of website and return payload with "image"
-      if (metadata.collection === 'picks_of_the_day' && payload.website_url) {
+      // If payload contain "website_url", create screenshot
+      // of website and add "image" to pick of the day item
+      if (payload.website_url) {
         // Create browserless response variable
         let browserlessResponse;
 
@@ -34,7 +37,7 @@ module.exports = (
         try {
           browserlessResponse = await axios({
             method: 'post',
-            url: `https://chrome.browserless.io/function?TOKEN=${env.BROWSERLESS_API_KEY}`,
+            url: `${env.BROWSERLESS_API_URL}function?TOKEN=${env.BROWSERLESS_API_TOKEN}`,
             headers: {
               'Content-Type': 'application/json',
             },
@@ -51,14 +54,11 @@ module.exports = (
 
           // If an error occurs, log it and inform team via Slack
         } catch (error) {
-          console.log(error);
           logger.error(`${HOOK_NAME} hook: Error: ${error.message}`);
           await postSlackMessage(
             `Achtung: Der Screenshot für einen Pick of the Day konnte nicht automatisch erstellt werden. Der Screenshoot muss nun manuell über den folgenden Link hinzugefügt werden: ${
               process.env.PUBLIC_URL
-            }admin/content/picks_of_the_day/${
-              (metadata.keys && metadata.keys[0]) || ''
-            }`
+            }admin/content/picks_of_the_day/${metadata.key || metadata.keys[0]}`
           );
         }
 
@@ -86,18 +86,21 @@ module.exports = (
             }
           );
 
-          // Return payload with image
+          // Create items service instance
+          const itemsService = new ItemsService(metadata.collection, {
+            accountability: context.accountability,
+            schema: context.schema,
+          });
+
+          // Add "image" to pick of the day item
           logger.info(
-            `${HOOK_NAME} hook: Set "image" at ${
-              metadata.keys && metadata.keys[0]
-                ? `"${metadata.collection}" item with ID "${metadata.keys[0]}"`
-                : `newly created "${metadata.collection}" item`
-            }`
+            `${HOOK_NAME} hook: Set "image" at "${
+              metadata.collection
+            }" item with ID "${metadata.key || metadata.keys[0]}"`
           );
-          return {
-            ...payload,
+          await itemsService.updateOne(metadata.key || metadata.keys[0], {
             image: imageId,
-          };
+          });
         }
       }
 
@@ -106,24 +109,21 @@ module.exports = (
       logger.error(`${HOOK_NAME} hook: Error: ${error.message}`);
       throw new BaseException(error.message, 500, 'UNKNOWN');
     }
-
-    // Otherwise just return payload
-    return payload;
   }
 
   /**
    * It sets the "image" field on newly created
    * pick of the day items, if necessary.
    */
-  filter('items.create', (payload, metadata, context) =>
-    handleFilter({ payload, metadata, context })
+  action('picks_of_the_day.items.create', ({ payload, ...metadata }, context) =>
+    handleAction({ payload, metadata, context })
   );
 
   /**
    * It sets the "image" field on updated pick
    * of the day items, if necessary.
    */
-  filter('items.update', (payload, metadata, context) =>
-    handleFilter({ payload, metadata, context })
+  action('picks_of_the_day.items.update', ({ payload, ...metadata }, context) =>
+    handleAction({ payload, metadata, context })
   );
 };
