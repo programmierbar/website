deb3856845a39753de436302777411bff54aff68
Fabi Fink <fabi.fink@gmail.com>
2023-02-02 11:37:26 +0100

    cms: refactor buzzsprout update.

--- directus-cms/extensions/hooks/buzzsproutApi/index.js
+++ directus-cms/extensions/hooks/buzzsproutApi/index.js
@@ -328,90 +328,94 @@ module.exports = (
       // Create is creation boolean
       const isCreation = typeof podcastItem.buzzsprout_id !== 'number';
 
-      if (
-        // If "buzzsprout_id" is not set and all required fields
-        // are set, create a podcast episode at Buzzsprout
-        (isCreation &&
-          podcastItem.status &&
-          (podcastItem.status !== 'published' || podcastItem.published_on) &&
-          podcastItem.type &&
-          podcastItem.number &&
-          podcastItem.title &&
-          podcastItem.description &&
-          podcastItem.cover_image &&
-          podcastItem.audio_file) ||
-        // Otherwise if "buzzsprout_id" is set and payload contains
-        // relevant changes, update podcast episode at Buzzsprout
-        (!isCreation &&
-          (payload.status ||
-            payload.published_on ||
-            payload.type ||
-            payload.number ||
-            payload.title ||
-            payload.description ||
-            payload.cover_image ||
-            payload.audio_file))
-      ) {
-        // Log info
-        logger.info(
-          `${HOOK_NAME} hook: Podcast creation or update required at Buzzsprout.`
-        );
-
-        // Get podcast data
-        const podcastData = await getPodcastData(podcastItem, {
-          context,
-        });
-
-        // Create or update Buzzsprout episode and get its data
-        const buzzsproutData = await handleBuzzsprout(podcastData, {
-          payload,
-          context,
-        });
-
-        // Create update data object
-        const updateData = {};
-
-        // If "buzzsprout_id" it not set, add it to update data
-        if (!podcastItem.buzzsprout_id) {
-          logger.info(
-            `${HOOK_NAME} hook: Set "buzzsprout_id" at "${
-              metadata.collection
-            }" item with ID "${metadata.key || metadata.keys[0]}"`
-          );
-          updateData.buzzsprout_id = buzzsproutData.id;
-        }
-
-        // If "audio_url" is not set or "audio_file", "type", "number"
-        // or "title" changed, add "audio_url" to update data
-        if (
-          !podcastItem.audio_url ||
-          payload.audio_file ||
+      // If "buzzsprout_id" is not set and all required fields
+      // are set, create a podcast episode at Buzzsprout
+      const createdAndRequiredFieldsSet =
+        isCreation &&
+        podcastItem.status &&
+        (podcastItem.status !== 'published' || podcastItem.published_on) &&
+        podcastItem.type &&
+        podcastItem.number &&
+        podcastItem.title &&
+        podcastItem.description &&
+        podcastItem.cover_image &&
+        podcastItem.audio_file;
+      // Otherwise if "buzzsprout_id" is set and payload contains
+      // relevant changes, update podcast episode at Buzzsprout
+      const updatedWithRelevantFields =
+        !isCreation &&
+        (payload.status ||
+          payload.published_on ||
           payload.type ||
           payload.number ||
-          payload.title
-        ) {
-          logger.info(
-            `${HOOK_NAME} hook: Set "audio_url" at "${
-              metadata.collection
-            }" item with ID "${metadata.key || metadata.keys[0]}"`
-          );
-          updateData.audio_url = buzzsproutData.audio_url;
-        }
+          payload.title ||
+          payload.description ||
+          payload.cover_image ||
+          payload.audio_file);
 
-        // If update data contains something, update podcast item
-        if (Object.keys(updateData).length) {
-          await podcastItemsService.updateOne(
-            metadata.key || metadata.keys[0],
-            updateData
-          );
-        }
-
-        // Otherwies log info
-      } else {
+      // If podcast creation or update is not required, return
+      if (!createdAndRequiredFieldsSet && !updatedWithRelevantFields) {
         logger.info(
-          `${HOOK_NAME} hook: Podcast creation or update at Buzzsprout is skipped.`
+          `${HOOK_NAME} hook: Podcast creation or update not required at Buzzsprout.`
         );
+        return;
       }
+
+      logger.info(
+        `${HOOK_NAME} hook: Podcast creation or update required at Buzzsprout.`
+      );
+
+      // Get podcast data
+      const podcastData = await getPodcastData(podcastItem, {
+        context,
+      });
+
+      // Create or update Buzzsprout episode and get its data
+      const buzzsproutData = await handleBuzzsprout(podcastData, {
+        payload,
+        context,
+      });
+
+      // Create update data object
+      const updateData = {};
+
+      // If "buzzsprout_id" it not set, add it to update data
+      if (!podcastItem.buzzsprout_id) {
+        logger.info(
+          `${HOOK_NAME} hook: Set "buzzsprout_id" at "${
+            metadata.collection
+          }" item with ID "${metadata.key || metadata.keys[0]}"`
+        );
+        updateData.buzzsprout_id = buzzsproutData.id;
+      }
+
+      // If "audio_url" is not set or "audio_file", "type", "number"
+      // or "title" changed, add "audio_url" to update data
+      if (
+        !podcastItem.audio_url ||
+        payload.audio_file ||
+        payload.type ||
+        payload.number ||
+        payload.title
+      ) {
+        logger.info(
+          `${HOOK_NAME} hook: Set "audio_url" at "${
+            metadata.collection
+          }" item with ID "${metadata.key || metadata.keys[0]}"`
+        );
+        updateData.audio_url = buzzsproutData.audio_url;
+      }
+
+      // If No update data is set, return
+      if (!Object.keys(updateData).length) {
+        return;
+      }
+
+      // If update data contains something, update podcast item
+      await podcastItemsService.updateOne(
+        metadata.key || metadata.keys[0],
+        updateData
+      );
 
       // Handle unknown errors
     } catch (error) {
