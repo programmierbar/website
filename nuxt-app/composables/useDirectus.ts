27acd796c067824ede07456ad3c2ea6d6c876ce8
david_lotum <koschitzki@lotum.de>
2024-02-23 15:35:28 +0100

    feat: add directus calls for podcast page and top tags

--- nuxt-app/composables/useDirectus.ts
+++ nuxt-app/composables/useDirectus.ts
@@ -6,12 +6,22 @@ export type LatestPodcasts = Pick<
     DirectusPodcastItem,
     'id' | 'slug' | 'published_on' | 'type' | 'number' | 'title' | 'cover_image' | 'audio_url'
 >[]
+export type CollectionName = 'members' | 'speakers' | 'tags' | 'podcasts' | 'meetups' | 'picks_of_the_day'
+export type Tag = { name: string; count: number }
 
 export function useDirectus() {
     async function getHomepage() {
         return await directus.request(
             readSingleton('home_page', {
                 fields: ['*', 'video.*'],
+            })
+        )
+    }
+
+    async function getPodcastPage() {
+        return await directus.request(
+            readSingleton('podcast_page', {
+                fields: ['*', 'cover_image.*'],
             })
         )
     }
@@ -56,12 +66,23 @@ export function useDirectus() {
         )
     }
 
-    async function getLatestPodcasts() {
+    async function getLatestPodcasts(limit: number = 10) {
         return await directus.request(
             readItems('podcasts', {
-                fields: ['id', 'slug', 'published_on', 'type', 'number', 'title', 'cover_image.*', 'audio_url'],
+                fields: [
+                    'id',
+                    'slug',
+                    'published_on',
+                    'type',
+                    'number',
+                    'title',
+                    'cover_image.*',
+                    'audio_url',
+                    'tags.tag.id',
+                    'tags.tag.name',
+                ],
                 sort: ['-published_on'],
-                limit: 10,
+                limit: limit,
             })
         )
     }
@@ -74,6 +95,28 @@ export function useDirectus() {
         )
 
         return Number(result.pop()?.count)
+    }
+
+    async function getTopTagsForCollection(collection: CollectionName, limit: number = 25) {
+        const tagCounts: { [key: string]: number } = {}
+        const tagItems = await directus.request(readItems(collection, { fields: ['tags.tag.name'], limit: -1 }))
+
+        for (const item of tagItems) {
+            for (const tagContainer of item.tags) {
+                const tag = tagContainer.tag
+                if (tag) {
+                    tagCounts[tag.name] = (tagCounts[tag.name] || 0) + 1
+                }
+            }
+        }
+
+        const tagsWithCounts: Tag[] = Object.keys(tagCounts).map((key) => ({
+            name: key,
+            count: tagCounts[key],
+        }))
+        const sortedTags = tagsWithCounts.sort((a, b) => b.count - a.count)
+
+        return sortedTags.slice(0, limit)
     }
 
     async function getMembers(filter: QueryFilter<Collections, DirectusMemberItem>) {
@@ -120,6 +163,7 @@ export function useDirectus() {
     return {
         getHomepage,
         getPicksOfTheDayPage,
+        getPodcastPage,
         getAboutPage,
         getPrivacyPage,
         getImprintPage,
@@ -128,5 +172,6 @@ export function useDirectus() {
         getLatestPodcasts,
         getPodcastCount,
         getPicksOfTheDay,
+        getTopTagsForCollection,
     }
 }
