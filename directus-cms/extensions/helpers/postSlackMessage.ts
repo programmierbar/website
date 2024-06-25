7b52f2f25be1ec2bef4db3162194cde0940c7770
david_lotum <koschitzki@lotum.de>
2024-06-25 15:25:16 +0200

    refac(slackMessage): rewrite function from js to ts

--- directus-cms/extensions/helpers/postSlackMessage.ts
+++ directus-cms/extensions/helpers/postSlackMessage.ts
@@ -1,20 +1,20 @@
-const { WebClient } = require('@slack/web-api');
+import { WebClient } from "@slack/web-api";
 
 // Create Slack web client instance
-const webClient = new WebClient(process.env.SLACK_BOT_TOKEN);
+const webClient = new WebClient(process.env.SLACK_BOT_TOKEN as string);
 
 /**
  * A helper function that posts a Slack message to a specific channel.
  *
  * @param text The text to send.
  */
-module.exports = async function postSlackMessage(text) {
+export async function postSlackMessage(text: string): Promise<void> {
   try {
     await webClient.chat.postMessage({
-      channel: process.env.SLACK_CHANNEL_ID,
+      channel: process.env.SLACK_CHANNEL_ID as string,
       text,
     });
-  } catch (error) {
+  } catch (error: any) {
     throw new Error(`Slack: ${error.message}`);
   }
-};
+}
