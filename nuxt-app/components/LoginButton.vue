d111ea030ef17a93699dbdab58eae2fc7283cfca
david_lotum <koschitzki@lotum.de>
2024-07-02 15:19:49 +0200

    feat(login): add login and register buttons

---
+++ nuxt-app/components/LoginButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isClicked = ref(false)
+
+const handleClick = () => {
+    isClicked.value = true
+    setTimeout(() => {
+        isClicked.value = false
+    }, 300) // Duration of the animation
+}
+</script>
+
+<template>
+    <button
+        :class="[
+            'btn btn-primary rounded-full bg-gray-900 bg-opacity-50 text-base font-light italic leading-8 tracking-wide text-white hover:bg-white hover:text-gray-900 lg:text-2xl',
+            isClicked ? 'animate-grow-fade' : '',
+        ]"
+        @click="handleClick"
+    >
+        <NuxtLink to="/login"> Einloggen </NuxtLink>
+    </button>
+</template>
+
+<style scoped lang="postcss">
+@keyframes grow-fade {
+    0% {
+        box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
+    }
+    100% {
+        box-shadow: 0 0 20px 20px rgba(255, 255, 255, 0);
+    }
+}
+
+.animate-grow-fade {
+    animation: grow-fade 0.3s ease-out;
+}
+</style>
---
+++ nuxt-app/components/RegisterButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const isClicked = ref(false)
+
+const handleClick = () => {
+    isClicked.value = true
+    setTimeout(() => {
+        isClicked.value = false
+    }, 300) // Duration of the animation
+}
+</script>
+
+<template>
+    <button
+        :class="[
+            'btn btn-primary rounded-full border-4 text-base font-bold uppercase leading-8 tracking-[2px] text-lime hover:bg-lime hover:text-black active:translate-y-1 md:tracking-[4px] lg:text-2xl',
+            isClicked ? 'animate-grow-fade' : '',
+        ]"
+        @click="handleClick"
+    >
+        <NuxtLink to="/login"> Anmelden </NuxtLink>
+    </button>
+</template>
+
+<style scoped lang="postcss">
+@keyframes grow-fade {
+    0% {
+        box-shadow: 0 0 0 0 rgba(0, 255, 0, 0.7);
+    }
+    100% {
+        box-shadow: 0 0 20px 20px rgba(0, 255, 0, 0);
+    }
+}
+
+.animate-grow-fade {
+    animation: grow-fade 0.3s ease-out;
+}
+</style>
