69deddc2edc87509e4d0f2a9a39bcca5127bce17
david_lotum <koschitzki@lotum.de>
2024-07-17 08:53:21 +0200

    feat(pinia): add pinia to enable convenient storage usage in Vue

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -48,6 +48,7 @@ export default defineNuxtConfig({
         // // Sitemap Module for Nuxt
         // '@nuxtjs/sitemap',
         'nuxt-jsonld',
+        '@pinia/nuxt',
     ],
 
     // Router configuration: https://nuxtjs.org/docs/configuration-glossary/configuration-router
