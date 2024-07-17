d4fd4185c4e435d97afdd76a1a891f7479f615c2
david_lotum <koschitzki@lotum.de>
2024-07-17 08:56:54 +0200

    feat(profile-creation): add component + helper components for the first side of the profile creation

---
+++ nuxt-app/components/InputFieldWithHeadline.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+    heading: string
+    modelValue: string
+    required?: boolean
+}>()
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: string): void
+}>()
+
+const inputValue = computed({
+    get: () => props.modelValue,
+    set: (value) => emit('update:modelValue', value),
+})
+</script>
+
+<template>
+    <div class="free-text-field-with-headline">
+        <div class="mb-2 text-base font-bold text-white">
+            {{ heading }}
+            <span v-if="required" class="mandatory-star text-base font-bold text-lime">*</span>
+        </div>
+
+        <InputField v-model="inputValue" type="text" />
+    </div>
+</template>
---
+++ nuxt-app/components/ProfileCreationMainInfos.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import ProfilePicture from '~/components/ProfilePicture.vue'
+import { computed, ref, watch } from 'vue'
+
+defineProps<{
+    heading: string
+    introText: string
+}>()
+
+const emit = defineEmits(['validityChange'])
+const store = useProfileCreationStore()
+const { mainInfos } = storeToRefs(store)
+
+const firstName = ref(mainInfos.value.firstName ?? '')
+const lastName = ref(mainInfos.value.lastName ?? '')
+
+const isFormValid = computed(() => {
+    return firstName.value.trim() !== '' && lastName.value.trim() !== ''
+})
+
+watch(isFormValid, (valid) => {
+    emit('validityChange', valid)
+})
+
+function updateFirstName(value: string) {
+    store.updateMainInfos({ ...mainInfos.value, firstName: value })
+}
+
+function updateLastName(value: string) {
+    store.updateMainInfos({ ...mainInfos.value, lastName: value })
+}
+</script>
+
+<template>
+    <div class="profile-creation-intro">
+        <div class="flex-col items-center justify-center md:flex">
+            <div class="mb-2 mt-5 text-3xl font-semibold italic leading-[3rem] text-white md:text-4xl">
+                {{ heading }}
+            </div>
+        </div>
+
+        <div class="intro-text mb-2 mt-5 text-base font-light text-white md:text-4xl" v-html="introText"></div>
+        <ProfilePicture class="mb-10" />
+        <div class="flex w-full flex-col items-center justify-center">
+            <InputFieldWithHeadline
+                v-model="firstName"
+                class="input-field"
+                heading="Name"
+                required
+                @update:model-value="updateFirstName"
+            />
+            <InputFieldWithHeadline
+                v-model="lastName"
+                class="input-field"
+                heading="Nachname"
+                required
+                @update:model-value="updateLastName"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="postcss">
+.input-field {
+    @apply mb-8 w-80 text-base font-bold text-white md:w-120;
+}
+</style>
