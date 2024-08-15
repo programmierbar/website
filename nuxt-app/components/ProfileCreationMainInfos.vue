60cddbb8dade3032407b12822cdcfedf4e9cac59
Jan Gregor Emge-Triebel <emge.triebel@lotum.de>
2024-08-15 11:05:03 +0200

    Refactored profile picture component to be reusable

--- nuxt-app/components/ProfileCreationMainInfos.vue
+++ nuxt-app/components/ProfileCreationMainInfos.vue
@@ -30,6 +30,11 @@ function updateFirstName(value: string) {
 function updateLastName(value: string) {
     store.updateMainInfos({ ...mainInfos.value, lastName: value })
 }
+
+function updatePicture(file: File, previewUrl: string) {
+  store.updateProfilePicture(file, previewUrl)
+}
+
 </script>
 
 <template>
@@ -41,7 +46,12 @@ function updateLastName(value: string) {
         </div>
 
         <div class="intro-text mb-2 mt-5 text-base font-light text-white md:text-4xl" v-html="introText"></div>
-        <ProfilePicture class="mb-10" />
+        <div class="flex w-full flex-col items-center justify-center my-10">
+          <ProfilePicture class="h-24 w-24 md:h-64 md:w-64"
+                          :editable='true'
+                          :handler='updatePicture'
+          />
+        </div>
         <div class="flex w-full flex-col items-center justify-center">
             <InputFieldWithHeadline
                 v-model="firstName"
--- nuxt-app/components/ProfilePicture.vue
+++ nuxt-app/components/ProfilePicture.vue
@@ -1,44 +1,62 @@
-<script setup lang="ts">
-import EmptyProfilePicture from '~/assets/images/profile-picture-empty.svg'
-import { useProfileCreationStore } from '~/composables/useProfileCreationStore'
-import { ref } from 'vue'
+<script setup lang='ts'>
+import EmptyProfilePicture from '~/assets/images/profile-picture-empty.svg';
+import { ref } from 'vue';
+import type { DirectusFileItem } from '~/types';
 
-const store = useProfileCreationStore()
-const fileInput = ref<HTMLInputElement | null>(null)
+type CallbackFunction = (file: File, previewUrl: string) => void;
 
-const handleFileUpload = (event: Event) => {
-    const target = event.target as HTMLInputElement
-    if (target.files && target.files.length > 0) {
-        const file = target.files[0]
-        const previewUrl = URL.createObjectURL(file)
-        store.updateProfilePicture(file, previewUrl)
+const props = defineProps<{
+  editable: boolean,
+  image?: DirectusFileItem,
+  handler?: CallbackFunction,
+}>();
+
+const fileInput = ref<HTMLInputElement | null>(null);
+const updatedImagePath = ref<string | null>(null);
+
+const handleFileSelection = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files.length > 0) {
+    const file = target.files[0];
+    const previewUrl = URL.createObjectURL(file);
+    updatedImagePath.value = previewUrl;
+    if (props.handler) {
+      props.handler(file, previewUrl);
     }
-}
+  }
+};
 
 const triggerFileInput = () => {
-    fileInput.value?.click()
-}
+  fileInput.value?.click();
+};
 </script>
 
 <template>
-    <div class="mt-10 flex flex-col items-center justify-center">
-        <div
-            class="profile-picture flex h-24 w-24 flex-col items-center justify-center rounded-full border-4 border-white bg-gradient-to-t from-white/20 to-white/80 md:h-64 md:w-64"
-        >
-            <img
-                v-if="store.profilePicture.previewUrl"
-                :src="store.profilePicture.previewUrl"
-                alt="Profile Picture"
-                class="h-full w-full rounded-full object-cover"
-            />
-            <EmptyProfilePicture v-else />
-        </div>
-        <input ref="fileInput" type="file" accept="image/*" class="hidden" @change="handleFileUpload" />
-        <PrimaryPbButton
-            class="mt-5 w-44 border-[1.4px] text-base font-light italic tracking-normal md:w-72 md:py-2 md:text-2xl md:tracking-wide"
-            @click="triggerFileInput"
-        >
-            Profilbild hochladen
-        </PrimaryPbButton>
+  <div class='flex flex-col items-center justify-center'>
+    <div
+      class='profile-picture flex flex-col items-center justify-center rounded-full border-4 border-white bg-gradient-to-t from-white/20 to-white/80 '
+    >
+      <DirectusImage
+        v-if='image && !updatedImagePath'
+        :image='image'
+        :sizes='""'
+        class='h-full w-full rounded-full object-cover'
+      />
+      <img
+        v-if='updatedImagePath'
+        :src='updatedImagePath'
+        alt='Profile Picture'
+        class='h-full w-full rounded-full object-cover'
+      />
+      <EmptyProfilePicture v-if='!updatedImagePath && !image' class='h-full w-full' />
     </div>
+    <input ref='fileInput' type='file' accept='image/*' class='hidden' @change='handleFileSelection' v-if='editable' />
+    <PrimaryPbButton
+      class='mt-5 w-44 border-[1.4px] text-base font-light italic tracking-normal md:w-72 md:py-2 md:text-2xl md:tracking-wide'
+      @click='triggerFileInput'
+      v-if='editable'
+    >
+      Profilbild hochladen
+    </PrimaryPbButton>
+  </div>
 </template>
