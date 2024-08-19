c91658697f7ad8329365d3134ce59a66ab709f18
david_lotum <koschitzki@lotum.de>
2024-08-19 14:36:00 +0200

    feat(icons): adapt profile section to be responsive

--- nuxt-app/components/ProfileSection.vue
+++ nuxt-app/components/ProfileSection.vue
@@ -1,14 +1,16 @@
-<template>
-  <h2 class='mb-8 mt-16 font-bold text-4xl'>{{ title }}</h2>
-  <p v-if='content' class='text-3xl font-light'>{{ content }}</p>
-  <InnerHtml v-else-if='html' :html='html' class='text-3xl font-light'/>
-  <slot></slot>
-</template>
-
-<script setup lang='ts'>
-const props = defineProps<{
-  title: string,
-  html?: string,
-  content?: string,
-}>();
+<script setup lang="ts">
+defineProps<{
+    title: string
+    html?: string
+    content?: string
+}>()
 </script>
+
+<template>
+    <div class="profile-section">
+        <h2 class="mb-6 mt-16 text-xl font-bold md:text-4xl">{{ title }}</h2>
+        <p v-if="content" class="text-base font-light md:text-3xl">{{ content }}</p>
+        <InnerHtml v-else-if="html" :html="html" class="text-base font-light md:text-3xl" />
+        <slot></slot>
+    </div>
+</template>
