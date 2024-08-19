7e28f8d5ba40b976051e2ce06d28d36c5da4b4a3
david_lotum <koschitzki@lotum.de>
2024-08-19 14:37:10 +0200

    feat(profileHeader): let profile header display all information and design it closer to figma

--- nuxt-app/components/ProfileHeader.vue
+++ nuxt-app/components/ProfileHeader.vue
@@ -1,24 +1,23 @@
-<template>
-  <div class='flex mb-6 items-center'>
-    <div class='h-24 w-24 md:h-64 md:w-64'>
-      <ProfilePicture :image='profile.profile_image' />
-    </div>
-    <div class='h-full flex flex-col pl-7'>
-      <h1 class='font-bold text-4xl'>
-        {{ profile.first_name }} {{ profile.last_name }}
-      </h1>
-      <p class='text-2xl font-light italic'>
-        🎂 dabei seit
-        <NuxtTime :datetime='profile.date_created_prepared' month='long' day='numeric' year='numeric' />
-      </p>
-    </div>
-  </div>
-</template>
+<script setup lang="ts">
+import type { DirectusProfileItem } from '~/types'
 
-<script setup lang='ts'>
-import type { DirectusProfileItem } from '~/types';
-
-const props = defineProps<{
-  profile: DirectusProfileItem,
-}>();
+defineProps<{
+    profile: DirectusProfileItem
+}>()
 </script>
+
+<template>
+    <div class="mb-6 flex flex-row items-center">
+        <div class="h-24 w-24 md:h-64 md:w-64">
+            <ProfilePicture :image="profile.profile_image" />
+        </div>
+        <div class="flex h-full flex-col gap-6 pl-7">
+            <h1 class="text-4xl font-bold">{{ profile.first_name }} {{ profile.last_name }}</h1>
+            <p v-if="profile.display_name" class="text-3xl font-light italic">@{{ profile.display_name }}</p>
+            <p class="text-2xl font-light italic">
+                🎂 Dabei seit dem
+                <NuxtTime :datetime="profile.date_created_prepared" month="long" day="numeric" year="numeric" />
+            </p>
+        </div>
+    </div>
+</template>
