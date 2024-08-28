3a2799a3567bb9db6df9cd6f8cfa9def896adb7d
Jan Gregor Emge-Triebel <emge.triebel@lotum.de>
2024-08-28 16:01:17 +0200

    Added empty picks of the day list as workaround

--- directus-cms/extensions/directus-extension-programmierbar-bundle/src/buzzsprout/handlers/podcastData.ts
+++ directus-cms/extensions/directus-extension-programmierbar-bundle/src/buzzsprout/handlers/podcastData.ts
@@ -18,45 +18,50 @@ export async function getPodcastData(
 ): Promise<PodcastData> {
     const { logger, ItemsService } = dependencies
 
-    // Log start info
-    logger.info(`${HOOK_NAME} hook: Query podcast data from Directus`)
+    let pickOfTheDayItems: PickOfTheDay[] = [];
 
     // !!!!
-    // We are manually adding admin permissions here to force the proper accountability
-    // du to this bug: https://github.com/directus/directus/issues/21689
+    // This will currently fail due to permission issues, most likely
+    // due to this bug: https://github.com/directus/directus/issues/21689
+    try {
+        // Log start info
+        logger.info(`${HOOK_NAME} hook: Query podcast data from Directus`)
 
-    // Create member items service instance
-    const memberItemsService = new ItemsService('members', {
-        accountability: { ...context.accountability, admin: true},
-        schema: context.schema,
-    })
-
-    // Create speaker items service instance
-    const speakerItemsService = new ItemsService('speakers', {
-        accountability: { ...context.accountability, admin: true},
-        schema: context.schema,
-    })
-
-    // Create pick of the day items service instance
-    const pickOfTheDayItemsService = new ItemsService('picks_of_the_day', {
-        accountability: { ...context.accountability, admin: true},
-        schema: context.schema,
-    })
-
-    // Log info
-    logger.info(`${HOOK_NAME} hook: Query pick of the day items from Directus`)
-
-    // Get pick of the day items with member or speaker
-    const pickOfTheDayItems: PickOfTheDay[] = await Promise.all(
-        podcastItem.picks_of_the_day!.map(async (pick) => {
-            const pickOfTheDay = await pickOfTheDayItemsService.readOne(pick.id)
-            return {
-                ...pickOfTheDay,
-                member: pickOfTheDay.member && (await memberItemsService.readOne(pickOfTheDay.member)),
-                speaker: pickOfTheDay.speaker && (await speakerItemsService.readOne(pickOfTheDay.speaker)),
-            }
+        // Create member items service instance
+        const memberItemsService = new ItemsService('members', {
+            accountability: context.accountability,
+            schema: context.schema,
         })
-    )
+
+        // Create speaker items service instance
+        const speakerItemsService = new ItemsService('speakers', {
+            accountability: context.accountability,
+            schema: context.schema,
+        })
+
+        // Create pick of the day items service instance
+        const pickOfTheDayItemsService = new ItemsService('picks_of_the_day', {
+            accountability: context.accountability,
+            schema: context.schema,
+        })
+
+        // Log info
+        logger.info(`${HOOK_NAME} hook: Query pick of the day items from Directus`)
+
+        // Get pick of the day items with member or speaker
+        pickOfTheDayItems = await Promise.all(
+            podcastItem.picks_of_the_day!.map(async (pick) => {
+                const pickOfTheDay = await pickOfTheDayItemsService.readOne(pick.id)
+                return {
+                    ...pickOfTheDay,
+                    member: pickOfTheDay.member && (await memberItemsService.readOne(pickOfTheDay.member)),
+                    speaker: pickOfTheDay.speaker && (await speakerItemsService.readOne(pickOfTheDay.speaker)),
+                }
+            })
+        )
+    } catch (error: any) {
+        logger.error(`${HOOK_NAME} hook: Could not query picks of the day "${error.message}"`)
+    }
 
     let tagItems: Tag[] = []
     try {
