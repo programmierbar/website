e70057178b0742465a3f9b3591826e5c4b89239d
Jan Gregor Emge-Triebel <emge.triebel@lotum.de>
2024-12-03 13:58:59 +0100

    Added a redirect for flutterday

---
+++ nuxt-app/server/middleware/flutterday.ts
@@ -0,0 +1,21 @@
+export default eventHandler(function(event) {
+  const toMatch = 'flutterday.programmier.bar';
+  const requestHost = event.headers.get('host') || '';
+
+  if (!requestHost.startsWith(toMatch)) {
+    return;
+  }
+
+  const host = 'https://programmier.bar';
+  const path = '/meetup/flutter-day-2024';
+
+  const redirectUrl = `${host}${path}`;
+
+  // Set the response status and location header for redirection
+  // And end the response to complete the redirection
+  event.node.res.writeHead(302, {
+    Location: redirectUrl,
+  });
+
+  event.node.res.end();
+});
