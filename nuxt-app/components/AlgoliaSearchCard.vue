ad91f0e389cbe9f7cfaca3c75df6cf341c51664d
Jan Gregor Emge-Triebel <emge.triebel@lotum.de>
2025-01-16 15:50:55 +0100

    Feature parity between AlgoliaSearchCard and legacy SearchCard

--- nuxt-app/components/AlgoliaSearchCard.vue
+++ nuxt-app/components/AlgoliaSearchCard.vue
@@ -12,23 +12,24 @@
           />
 
             <div>
-                <!-- Type and date -->
-                <div class="text-xs font-light italic text-white md:text-base lg:text-xl">
+              <!-- Type and date -->
+              <div class="text-xs font-light italic text-white md:text-base lg:text-xl">
+                {{ typeAndDate }}
+              </div>
 
-                </div>
+              <!-- Heading -->
+              <h2
+                class="mt-2 line-clamp-2 text-xl font-black leading-snug text-white md:text-3xl lg:mt-5 lg:text-4xl lg:leading-snug"
+              >
+                {{ heading }}
+                <LeaveSiteIcon v-if="isExternalUrl" class="ml-1 mt-px inline-block h-4 text-white xl:h-6" />
+              </h2>
 
-                <!-- Heading -->
-                <h2
-                    class="mt-2 line-clamp-2 text-xl font-black leading-snug text-white md:text-3xl lg:mt-5 lg:text-4xl lg:leading-snug"
-                >
-                    {{ item.title }}
-                </h2>
-
-                <!-- Description -->
-                <p
-                    class="mt-4 line-clamp-4 text-sm font-light leading-normal text-white md:text-lg lg:mt-8 lg:text-xl lg:leading-relaxed"
-                    v-html="item.description"
-                />
+              <!-- Description -->
+              <p
+                class="mt-4 line-clamp-4 text-sm font-light leading-normal text-white md:text-lg lg:mt-8 lg:text-xl lg:leading-relaxed"
+                v-html="description"
+              />
             </div>
         </nuxt-link>
     </div>
@@ -36,17 +37,78 @@
 
 <script setup lang="ts">
 import LeaveSiteIcon from '~/assets/icons/leave-site.svg'
-import type { SearchItem } from '~/types'
 import { getFullPodcastTitle, getFullSpeakerName } from 'shared-code'
 import { computed } from 'vue'
-import DirectusImage from './DirectusImage.vue'
 
 const props = defineProps<{
     item: {
+      _type: string,
+      published_on: string,
+
       url: string,
+      name: string,
       title: string,
       description: string,
       image: string,
+
     }
 }>()
+
+// Refactor this separate by type
+
+// Create URL or path for <a> element
+const urlOrPath = computed(() =>
+  props.item._type === 'podcast'
+    ? `/podcast/${props.item.slug}`
+    : props.item._type === 'meetup'
+      ? `/meetup/${props.item.slug}`
+      : props.item._type === 'pick_of_the_day'
+        ? props.item.website_url
+        : `/hall-of-fame/${props.item.slug}`
+)
+
+// Create is external URL boolean
+const isExternalUrl = computed(() => props.item._type === 'pick_of_the_day')
+
+// Get image depending on item typ
+const image = computed(() =>
+  props.item._type === 'podcast' || props.item._type === 'meetup'
+    ? props.item.cover_image
+    : props.item._type === 'pick_of_the_day'
+      ? props.item.image
+      : props.item.profile_image
+)
+
+// Create type and date depending on item typ
+const typeAndDate = computed(
+  () =>
+    (props.item._type === 'podcast'
+      ? 'Podcast'
+      : props.item._type === 'meetup'
+        ? 'Meetup'
+        : props.item._type === 'pick_of_the_day'
+          ? 'Pick of the Day'
+          : 'Speaker') +
+    ' // ' +
+    new Date(props.item.published_on).toLocaleDateString('de-DE', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })
+)
+
+
+// Create heading depending on item typ
+const heading = computed(() =>
+  props.item._type === 'podcast'
+    ? getFullPodcastTitle(props.item)
+    : props.item._type === 'meetup'
+      ? props.item.title
+      : props.item._type === 'pick_of_the_day'
+        ? props.item.name
+        : getFullSpeakerName(props.item)
+)
+
+const description = computed(() => props.item.description.replace(/<[^<>]+>/g, ''))
+
 </script>
