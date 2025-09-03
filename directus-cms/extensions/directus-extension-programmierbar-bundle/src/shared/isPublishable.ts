0ca4e5d15251a09b747c98ad2146d492cdb1f280
Jan Gregor Emge-Triebel <emge.triebel@lotum.de>
2025-09-03 13:48:51 +0200

    Adapted isPublishable test to check for more complex conditions

--- directus-cms/extensions/directus-extension-programmierbar-bundle/src/shared/isPublishable.ts
+++ directus-cms/extensions/directus-extension-programmierbar-bundle/src/shared/isPublishable.ts
@@ -11,6 +11,10 @@ interface Field {
                 {
                     status: {
                         _eq: string
+                    },
+                    type: {
+                        _eq: string
+                        _in: Array<string>
                     }
                 }[]
             >
@@ -20,6 +24,19 @@ interface Field {
 
 // eslint-disable-next-line no-unused-vars
 type LoggerFunction = (message: string) => void
+
+const isRuleForPublished = function(rule: any): boolean {
+    return (rule.status && rule.status._eq && rule.status._eq === 'published') || false;
+}
+const isRuleApplicable = function(rule: any, item: Record<string, any>): boolean {
+
+    if (rule.type && rule.type._in && item.type && rule.type._in.includes(item.type)) {
+        return true;
+    }
+
+    return false;
+}
+
 
 export function isPublishable(item: Record<string, any>, fields: Field[], logger?: LoggerFunction) {
     const requiredFieldsAreSet = fields.every((field) => {
@@ -39,14 +56,21 @@ export function isPublishable(item: Record<string, any>, fields: Field[], logger
                 return (
                     condition.required &&
                     condition.rule &&
-                    condition.rule._and &&
-                    condition.rule._and.some(
-                        (rule) => rule.status && rule.status._eq && rule.status._eq === 'published'
+                    (
+                        isRuleForPublished(condition.rule) ||
+                        (
+                            condition.rule._and &&
+                            condition.rule._and.some(
+                                (rule) => isRuleForPublished(rule)
+                            ) &&
+                            condition.rule._and.some(
+                                (rule) => isRuleApplicable(rule, item)
+                            )
+                        )
                     )
                 )
-            })
-
-            ;(() => logger?.('is required on published ' + isRequiredOnPublished))()
+            });
+            (() => logger?.('is required on published ' + isRequiredOnPublished))()
         }
         const isOptional = !isRequiredInSchema && !isRequiredOnPublished
 
